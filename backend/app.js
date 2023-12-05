ee62798b161f6709fddeae3b76832ab80c6b13ef
AMARMG04 <117839448+AMARMG04@users.noreply.github.com>
2023-12-05 15:29:58 +0530

    Changes in the frontend

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors({ 
-  origin: "https://theweekendcoders.vercel.app/contact",
+  origin: "https://theweekendcoders.vercel.app/",
   methods: ["GET", "POST", "OPTIONS", "PUT"]
 }));
 // Enable CORS for all routes
