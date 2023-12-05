22598edd6a54765969bb2fa2e0a9b09b3a1f9f5b
AMARMG04 <117839448+AMARMG04@users.noreply.github.com>
2023-12-05 15:19:11 +0530

    Changes in the frontend

--- src/app/components/ContactForm.tsx
+++ src/app/components/ContactForm.tsx
@@ -44,7 +44,7 @@ const ContactForm: React.FC = () => {
             // Make a POST request to the backend endpoint
             setLoading(true);
             const response = await axios.post(
-                "api/submit-form",
+                "/api/submit-form",
                 data
             );
             setStep((prevStep) => prevStep + 1);
