5f927ca6dd5b2f4c8d06a0bd508cadeb5ea66f1a
AMARMG04 <117839448+AMARMG04@users.noreply.github.com>
2023-12-07 17:47:35 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ module.exports = {
           source: "/submit-form",
           headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "https://theweekendcoders.vercel.app" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
             { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
             { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
           ]
